f7e25056ab8fc1ee757afc1e3ab4dd1d8039e1a7
Sebastian Gula <sebastian.gula@hotmail.com>
2019-04-26 21:18:59 +0200

    Added helper for working with dictionaries.

---
+++ Assets/Entities/Shared/Scripts/DictionaryInfoHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class DictionaryInfoHelper
+{
+    private static IList<string> GetFolderNames(string path)
+    {
+        var dir = new DirectoryInfo(path);
+        var info = dir.GetDirectories();
+
+        return info.Select(i => i.Name).ToList();
+    }
+
+    private static IList<string> GetFilesNames(string path)
+    {
+        var dir = new DirectoryInfo(path);
+        var info = dir.GetFiles();
+
+        return info.Select(i => i.Name).ToList();
+    }
+}
