b519b906081e1fb30b01b523b6c3e2b43ee8b0eb
Sebastian Gula <sebastian.gula@hotmail.com>
2019-05-17 19:31:27 +0200

    Changed DungeonGenerator to connect generated rooms.
    At the beginning the script finds all the generated rooms, removes the smallest rooms so that the size of the board matches the settings of the generator and finally combines all the other rooms to create one big one.

--- Assets/Entities/BoardGenerators/Dungeon/Scripts/DungeonGenerator.cs
+++ Assets/Entities/BoardGenerators/Dungeon/Scripts/DungeonGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class DungeonGenerator : BoardGeneratorStrategy
 {
@@ -12,15 +14,23 @@ public class DungeonGenerator : BoardGeneratorStrategy
     private int _columns;
     private int _previousRowAverageY;
 
-    public override BoardField[,] GenerateBoard(int width, int height, int borderSize, int minimumAvailableSurfacePercent, int maximumAvailableSurfacePercent)
+    public override BoardField[,] GenerateBoard(int width, int height, int borderSize, int minimumAvailableSurfacePercent)
     {
         _sum = 0;
         _columns = 0;
         _previousRowAverageY = DistanceFromBorder.Max;
 
-        Initialize(width, height, borderSize, minimumAvailableSurfacePercent, maximumAvailableSurfacePercent);
+        Initialize(width, height, borderSize, minimumAvailableSurfacePercent);
         ClearBoard(width, height);
         GenerateRooms(width, height, borderSize);
+
+        var rooms = FilterRooms(FindRooms());
+        var list = FindClosestPointsBeetwanRooms(rooms);
+
+        foreach (var points in list.GroupBy(x => x.RoomNumber).Select(g => g.Aggregate((p, n) => p.Distance < n.Distance ? p : n)).ToList())
+        {
+            ConnectTwoPoins(points.PointA, points.PointB);
+        }
 
         return board;
     }
@@ -59,7 +69,7 @@ public class DungeonGenerator : BoardGeneratorStrategy
                 {
                     for (var y = startY; y < endY; y++)
                     {
-                        board[x, y] = BoardField.Empty;
+                        board[x, y] = BoardField.Floor;
                     }
                 }
 
@@ -102,7 +112,7 @@ public class DungeonGenerator : BoardGeneratorStrategy
 
             var field = board[x, y];
 
-            if (field == BoardField.Empty)
+            if (field == BoardField.Floor)
             {
                 return true;
             }
@@ -125,5 +135,96 @@ public class DungeonGenerator : BoardGeneratorStrategy
     {
         _columns++;
         _sum += y;
+    }
+
+    private List<Room> FindRooms()
+    {
+        var rooms = new List<Room>();
+        int roomNumber = 3;
+
+        for (var x = 0; x < boardWidth; x++)
+        {
+            for (var y = 0; y < boardHeight; y++)
+            {
+                var roomFields = GetRoom(x, y, roomNumber, new List<Vector2>());
+
+                if (roomFields.Count > 0)
+                {
+                    rooms.Add(new Room(roomNumber, roomFields));
+                    roomNumber++;
+                }
+            }
+        }
+
+        return rooms.OrderByDescending(room => room.RoomFields.Count).ToList();
+    }
+
+    private List<Room> FilterRooms(List<Room> rooms)
+    {
+        var filteredRooms = new List<Room>();
+        var sum = 0;
+        var i = 0;
+
+        while (sum < minimumFloorTiles && i < rooms.Count)
+        {
+            var room = rooms[i];
+            sum += room.RoomFields.Count;
+            filteredRooms.Add(room);
+            i++;
+        }
+
+        ClearOtherRooms(filteredRooms.Select(r => r.RoomNumber).ToList());
+
+        return filteredRooms;
+    }
+
+    private List<RoomConnectionPoints> FindClosestPointsBeetwanRooms(List<Room> rooms)
+    {
+        var list = new List<RoomConnectionPoints>();
+
+        for (var i = 0; i < rooms.Count; i++)
+        {
+            var roomA = rooms[i];
+
+            for (var j = i + 1; j < rooms.Count; j++)
+            {
+                list.Add(FindClosestPointsBeetweanTwoRooms(roomA, rooms[j]));
+            }
+        }
+
+        return list;
+    }
+
+    private RoomConnectionPoints FindClosestPointsBeetweanTwoRooms(Room roomA, Room roomB)
+    {
+        var list = new List<RoomConnectionPoints>();
+        var tree = new KdTree();
+        tree.AddAll(roomB.RoomFields);
+
+        foreach (var field in roomA.RoomFields)
+        {
+            var closestPoint = tree.FindClosest(field);
+            var distance = Vector2.Distance(field, closestPoint);
+
+            list.Add(new RoomConnectionPoints
+            {
+                RoomNumber = roomA.RoomNumber,
+                PointA = field,
+                PointB = closestPoint,
+                Distance = distance
+            });
+        }
+
+        return list.Aggregate((p, n) => p.Distance < n.Distance ? p : n);
+    }
+
+    private void ConnectTwoPoins(Vector2 pointA, Vector2 pointB)
+    {
+        var aStar = new AStar();
+
+        foreach (var point in aStar.FindShortestPathAsList(pointA, pointB))
+        {
+            board[(int)point.x, (int)point.y] = BoardField.Floor;
+        }
     }
 }
