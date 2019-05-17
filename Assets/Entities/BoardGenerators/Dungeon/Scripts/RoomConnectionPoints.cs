7ca3321e5de52f292fbcad6881b65b8a15b37e3b
Sebastian Gula <sebastian.gula@hotmail.com>
2019-05-17 19:24:21 +0200

    Added class for DungeonGenerator

---
+++ Assets/Entities/BoardGenerators/Dungeon/Scripts/RoomConnectionPoints.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RoomConnectionPoints
+{
+    public int RoomNumber { get; set; }
+    public Vector2 PointA { get; set; }
+    public Vector2 PointB { get; set; }
+    public float Distance { get; set; }
+}
